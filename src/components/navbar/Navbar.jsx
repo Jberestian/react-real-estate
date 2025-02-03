e720fb86cbddfc31f7bc16dd51fca82bfcc1c967
Jberestian <jberestian@gmail.com>
2025-02-03 12:34:57 +0200

    finish navbar menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import "./navbar.scss";
+import { useState } from "react";
 
 function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <nav>
       <div className="left">
@@ -20,7 +22,20 @@ function Navbar() {
         </a>
 
         <div className="menuIcon">
-          <img src="/menu.png" alt="" />
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+
+        <div className={`menu ${open ? "menu active" : "menu"}`}>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Contact</a>
+          <a href="">Agents</a>
+          <a href="">Sign in</a>
+          <a href="">Sign up</a>
         </div>
       </div>
     </nav>
