8b16f8f7efd392f25dd587905816d76708944537
Jberestian <jberestian@gmail.com>
2025-02-06 12:32:12 +0200

    added useState and some functionality to the searchBar component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react";
 import "./searchBar.scss";
 
 function SearchBar() {
+    const [query, setQuery] = useState({
+        type: "buy",
+        location: "",
+        minPrice: 0,
+        maxPrice:0,
+    })
   return (
     <div className="searchBar">
-      <div className="type"></div>
+      <div className="type">
+        <button onClick={()=>}>Buy</button>
+        <button>Rent</button>
+      </div>
       <form action="">
         <input type="text" name="location" placeholder="city Location" />
-        <input type="number" name="minPrice" placeholder="Min price" />
-        <input type="number" name="maxPrice" placeholder="Max price" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={10000000}
+          placeholder="Min price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={10000000}
+          placeholder="Max price"
+        />
+
+        <button>
+          <img src="./search.png" alt="" />
+        </button>
       </form>
     </div>
   );
