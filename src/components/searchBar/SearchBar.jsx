086d4f5fc3cf07ebefbd3051b6f32d1c9042f57d
Jberestian <jberestian@gmail.com>
2025-02-06 16:08:27 +0200

    add styles for all screens for searhBar component

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,18 +1,32 @@
 import { useState } from "react";
 import "./searchBar.scss";
 
+const types = ["buy", "rent"];
+
 function SearchBar() {
-    const [query, setQuery] = useState({
-        type: "buy",
-        location: "",
-        minPrice: 0,
-        maxPrice:0,
-    })
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
+  };
+
   return (
     <div className="searchBar">
       <div className="type">
-        <button onClick={()=>}>Buy</button>
-        <button>Rent</button>
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
       </div>
       <form action="">
         <input type="text" name="location" placeholder="city Location" />
