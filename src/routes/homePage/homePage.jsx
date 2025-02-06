6315445c991f04f30f564723e9fb5882bb8ec559
Jberestian <jberestian@gmail.com>
2025-02-06 10:41:21 +0200

    added searchBar and some functionality

--- src/routes/homePage/homePage.jsx
+++ src/routes/homePage/homePage.jsx
@@ -1,10 +1,24 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./homePage.scss";
 
 function HomePage() {
   return (
     <div className="homePage">
-      <div className="textContainer">text</div>
-      <div className="imgContainer">img</div>
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem cumque
+            quod quaerat libero autem ut sed voluptatibus laudantium impedit aut
+            cum, qui debitis corrupti corporis. Voluptates, odit? Distinctio,
+            nobis inventore.
+          </p>
+          <SearchBar />
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
     </div>
   );
 }
